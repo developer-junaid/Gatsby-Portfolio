575920b491a4688fe77d8121d2c75dafc0f610ca
developer-junaid <qjunaid623@gmail.com>
2021-04-25 04:49:26 +0500

    Created ProjectDetails Template

---
+++ src/templates/project-details.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Img from "gatsby-image"
+import * as styles from "../styles/project-details.module.css"
+
+export default function ProjectDetails() {
+  return (
+    <Layout>
+      <div className={styles.details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={styles.featured}>{/* <Img fluid={} /> */}</div>
+        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+      </div>
+    </Layout>
+  )
+}
