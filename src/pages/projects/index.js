cf93b2b19b0b2d6e6cbe959d778db9d6809f77ca
developer-junaid <qjunaid623@gmail.com>
2021-04-25 02:55:45 +0500

    Rendered Data from Page Query (Projects)

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,15 +1,46 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
 
 // Route for this page should be /projects
-export default function Projects() {
+export default function Projects({ data }) {
+  // Data
+  const projects = data.allMarkdownRemark.nodes
+
+  // Return
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created.</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link key={project.id} to={`/projects/${project.frontmatter.slug}`}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// Page Query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+        }
+        id
+      }
+    }
+  }
+`
