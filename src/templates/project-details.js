15be3fb073ea657f2f7239c0dbc3577d584f7a28
developer-junaid <qjunaid623@gmail.com>
2021-04-25 06:04:12 +0500

    Made Template Dynamic

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -2,16 +2,47 @@ import React from "react"
 import Layout from "../components/Layout"
 import Img from "gatsby-image"
 import * as styles from "../styles/project-details.module.css"
+import { graphql } from "gatsby"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  // Data
+  const { html } = data.markdownRemark
+  const { title, stack, featured } = data.markdownRemark.frontmatter
+  console.log(data)
+
+  // Return
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
-        <div className={styles.featured}>{/* <Img fluid={} /> */}</div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div className={styles.featured}>
+          <Img fluid={featured.childImageSharp.fluid} />
+        </div>
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featured {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
